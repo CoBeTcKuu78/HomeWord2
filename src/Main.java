fc7d4cb517c0c61c627ca06e9b379509ffa16f3f
CoBeTcKuu <9437855@gmail.com>
2022-08-02 20:47:46 +0300

    коммит дз 2 часть 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte a = 12;
+        System.out.println(a);
+        short b = 135;
+        System.out.println(b);
+        int c = 1489321;
+        System.out.println(c);
+        long d = 19999374832894L;
+        System.out.println(d);
+        float e = 1.4f;
+        System.out.println(e);
+        double f = 18.987633372819d;
+        System.out.println(f);
+        char ch = 0;
+        System.out.println(ch);
+        int age = 17;
+        boolean  isAdult = age >= 18;
+        System.out.println(isAdult);
+
     }
 }
